973496b827cf79dadbead59184b5d79dc7a84d4f
kzembill <kzembill@gmail.com>
2023-11-26 12:06:00 -0500

    added a function

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -7,11 +7,13 @@ import "./Dictionary.css";
 export default function Dictionary () {
     let [keyword, setKeyword] = useState("");
 
+    function handleResponse(response) {
+        console.log(response.data[0]);
+    }
 
     function search(event) {
         event.preventDefault();
-        alert(`Searching for ${keyword} definition`);
-
+      
         
     let apiUrl = 'https://api.shecodes.io/dictionary/v1/define?word={keyword}&key={a56bd14t7ad00f6eo465f0e67d3dc8fa}';
     console.log(apiUrl);
@@ -21,6 +23,7 @@ export default function Dictionary () {
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
     }
+
     return (
     <div className="Dictionary">
         <form onSubmit={search}>
