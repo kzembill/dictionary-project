6dc648746bae65a795609f1783ca88b0a01a6a44
kzembill <kzembill@gmail.com>
2023-11-26 10:08:30 -0500

    added logo and css, deleted export default at bottom of app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
+import logo from './shecodes logo2.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <img src={logo} className="App=logo img fluid"
+        alt="logo" />
+      </header>
     </div>
   );
 }
 
-export default App;
