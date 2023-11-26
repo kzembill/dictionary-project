cffd38b15298ba04e469a73e34574ed010df5507
kzembill <kzembill@gmail.com>
2023-11-26 11:10:19 -0500

    corrected some punctuation errors that were messing with code

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,18 +4,22 @@ import "./Dictionary.css";
 
 export default function Dictionary () {
     let [keyword, setKeyword] = useState("");
+
+
     function search(event) {
-        event,preventDefault();
+        event.preventDefault();
         alert(`Searching for ${keyword} definition`);
     }
 
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
     }
-    return <div className="Dictionary">
+    return (
+    <div className="Dictionary">
         <form onSubmit={search}>
             <input type="search" onChange=
             {handleKeywordChange} />
         </form>
-    </div>;
+    </div>
+    );
 }
