b4097d6390199d6cd4d5d748f9ccaae1b8bb0c7f
kzembill <kzembill@gmail.com>
2023-11-26 10:11:37 -0500

    some errors did not allow deploy-fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './shecodes logo2.png';
+import logo from "./shecodes logo2.png";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App=logo img fluid"
+        <img src={logo} className="App-logo img-fluid"
         alt="logo" />
       </header>
     </div>
