8aabca15810d3ca4e02a7cce19f2bb7692a53a0e
kzembill <kzembill@gmail.com>
2023-11-27 11:39:50 -0500

    undid changes and re-entered my code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import logo from "./logo.png";
-import "./App.css";
+import logo from "./shecodes logo2.png";
+import './App.css';
+import React from "react";
 import Dictionary from "./Dictionary";
 
-export default function App() {
+function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
-        </header>
-        <main>
-          <Dictionary defaultKeyword="sunset" />
-        </main>
-        <footer className="App-footer">
-          <small>Coded by SheCodes</small>
-        </footer>
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid"
+        alt="logo" />
+      </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer"> 
+        <small>Coded by Kalli Zembillas</small>
+      </footer>
       </div>
     </div>
   );
 }
+
+export default App;
