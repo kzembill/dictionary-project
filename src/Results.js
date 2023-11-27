3a1d9cf258da328bd15e97e479f86d7d313b4871
kzembill <kzembill@gmail.com>
2023-11-27 14:01:33 -0500

    updated my return under my results component

--- src/Results.js
+++ src/Results.js
@@ -5,6 +5,7 @@ export default function Result () {
     return <div className="Results">
         <h2>{props.results.word}</h2>
         {props.results.meanings.map(function(meaning, index) {
+            return meaning.definitions[0].definition;
         })}
         </div>;
     } else {
