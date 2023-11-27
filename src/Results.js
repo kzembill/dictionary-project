f8c7b76bad308fb2221bccc1eccc36cfa5015f01
kzembill <kzembill@gmail.com>
2023-11-27 14:03:40 -0500

    added my parenthesis next to my return

--- src/Results.js
+++ src/Results.js
@@ -2,12 +2,14 @@ import React from "react";
 
 export default function Result () {
     if (props.results) {
-    return <div className="Results">
+    return (
+    <div className="Results">
         <h2>{props.results.word}</h2>
         {props.results.meanings.map(function(meaning, index) {
             return meaning.definitions[0].definition;
         })}
-        </div>;
+        </div>
+    );
     } else {
         return null;
     }
