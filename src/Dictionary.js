56739cd1521ef0264d7b6227e81b9cb59974cdc7
kzembill <kzembill@gmail.com>
2023-11-27 14:33:42 -0500

    adjusted my dictionary component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,12 +5,11 @@ import "./Dictionary.css";
 
 
 
-export default function Dictionary (props) {
-    let [keyword, setKeyword] = useState(props.defaultKeyword);
+export default function Dictionary () {
+    let [keyword, setKeyword] = useState("");
     let [results, setResults] = useState(null);
 
     function handleResponse(response) {
-        console.log(response.data[0]);
         setResults(response.data[0]);
     }
 
