bcd0ce6322019b936014f073711dca6946f8611e
kzembill <kzembill@gmail.com>
2023-11-27 11:21:26 -0500

    changed api key. my deploy is still failing-trying to figure out the issue

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -15,7 +15,7 @@ export default function Dictionary () {
         event.preventDefault();
       
         
-   let apiKey = "a56bd14t7ad00f6eo465f0e67d3dc8fa";
+   let apiKey = "bd79ao40tde3dec118ca46bc3e6dd55f";
    let apiUrl = 'https://api.shecodes.io/dictionary/v1/define?word={keyword}&key={apiKey}';
     axios.get(apiUrl).then(handleResponse);
     }
