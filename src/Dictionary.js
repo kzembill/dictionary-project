cef187edf04082a017a75833d2ecfb57c996c349
kzembill <kzembill@gmail.com>
2023-11-30 22:01:23 -0500

    removed the loaded and setLoaded

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,7 +8,6 @@ import "./Dictionary.css";
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
-  let [loaded, setLoaded] = useState(false);
   let [photos, setPhotos] = useState(null);
 
   function handleDictionResponse(response) {
