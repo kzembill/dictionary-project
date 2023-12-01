7f73a096c196f7e4c0dfe7c1d114873ec8359f0a
kzembill <kzembill@gmail.com>
2023-11-30 20:48:09 -0500

    forgot to define apikey

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -21,7 +21,7 @@ export default function Dictionary(props) {
   function search() {
     // documentation: https://dictionaryapi.dev/e
     let apiKey="a56bd14t7ad00f6eo465f0e67d3dc8fa";
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleDictionResponse);
 
     let pexelsApiKey =
